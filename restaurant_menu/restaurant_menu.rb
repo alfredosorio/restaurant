9b70b0f301c44187afa88ea6758d57fc9478626f
alfred.osorio <alfred.osorio@outlook.com>
2017-07-27 21:26:56 +1000

    Refactor code

--- restaurant_menu/restaurant_menu.rb
+++ restaurant_menu/restaurant_menu.rb
@@ -6,14 +6,15 @@ class MenuItem
     @category = category
     @description = description
     @price = price
-    @options = options
+    @course = nil
+    @options = nil
   end
 
-  attr_accessor :name, :category, :description, :price, :options
+  attr_accessor :name, :category, :description, :price, :course, :options
 end
 
 class Order
-  def initialize()
+  def initialize
     @items = []
   end
 
@@ -30,94 +31,119 @@ class Order
   end
 
   def bill
-    table = Terminal::Table.new headings: ['Name', 'Options', 'Price'] do |t|
+    table = Terminal::Table.new headings: ['Name', 'Course', 'Options', 'Price'] do |t|
       @items.each do |item|
-        t << [item.name, item.options, "$#{item.price}"]
+        t << [item.name, item.course.capitalize, item.options.capitalize, "$#{item.price}"]
       end
       t.add_separator
-      t << ['TOTAL','', total]
+      t << ['TOTAL','','', total]
     end
     table
   end
-end
 
-def display_food_menu
-  puts 'Food Menu:'
-  MENU_ITEMS.each_with_index do |menu_item, index|
-    user_index = index + 1
-    # Display item with index first, then name and price
-    if menu_item.category == "Food"
-      puts "#{user_index}. #{menu_item.name}: #{menu_item.description} ($#{menu_item.price})"
-    end
-  end
-end
+  def request_item(order)
+    loop do
+      display_food_menu
+      puts '' # Divider Line
+      display_drinks_menu      
+      print 'What would you like? '
+      choice = gets.chomp
+      # Stop looping if user pressed just enter
+      break if choice == ""
 
-def display_drinks_menu
-  puts 'Drinks Menu:'
-  MENU_ITEMS.each_with_index do |menu_item, index|
-    user_index = index + 1
-    # Display item with index first, then name and price
-    if menu_item.category == "Drink"
-      puts "#{user_index}. #{menu_item.name}: #{menu_item.description} ($#{menu_item.price})"
-    end    
-  end
-end
+      # User must choose an index number
+      user_index = choice.to_i
 
-def order_items(order)
-  display_food_menu
-  puts '' # Divider Line
-  display_drinks_menu
-  puts '' # Divider Line
-  loop do
-    puts 'What would you like?'
-    choice = gets.chomp
-    # Stop looping if user pressed just enter
-    break if choice == ""
+      # If the user entered in an invalid choice
+      if user_index == 0
+        "Invalid choice, please try again"
+        next # Loop through and ask again
+      end
 
-    # User must choose an index number
-    user_index = choice.to_i
+      index = user_index - 1 # Convert to zero-based index
+      menu_item = MENU_ITEMS[index]
 
-    # If the user entered in an invalid choice
-    if user_index == 0
-      "Invalid choice, please try again"
-      next # Loop through and ask again
-    end
+      # Add item to order
+      order << menu_item
 
-    index = user_index - 1 # Convert to zero-based index
-    menu_item = MENU_ITEMS[index]
+      # Repeat order back
+      puts "You have ordered the #{menu_item.name}."
+      puts '' # Divider Line
 
-    # Add item to order
-    order << menu_item
 
-    # Repeat order back
-    puts "You have ordered the #{menu_item.name}."
-    puts '' # Divider Line
-
-    # puts "Do you have any special options with your #{menu_item.name}?"
-    # puts '' # Divider Line
-    # user_options = gets.chomp
-    # order << user_options
-    
-    # Which course?
-    puts "Would you like the #{menu_item.name} for your [e]ntree, [m]ain or [d]essert?"
-      loop do
-      course_choice = gets.chomp.downcase
-        if course_choice == 'e'
-          puts 'You have chosen an entree'
-          return
-        elsif course_choice == 'm'
-          puts 'You have chosen an main'
-          return
-        elsif course_choice == 'd'
-          puts 'You have chosen a dessert'
-          return    
+      # Request course
+      print "Would you like the #{menu_item.name} for your [e]ntree, [m]ain or [d]essert? "
+      loop do # Refactor and make shorter
+        choice = gets.chomp.downcase
+        case choice
+        when 'e'
+          puts "The #{menu_item.name} will be served as part of your entree."
+          menu_item.course = 'entree'
+          break
+        when 'm'
+          puts "The #{menu_item.name} will be served as part of your main."
+          menu_item.course = 'main'
+          break
+        when 'd'
+          puts "The #{menu_item.name} will be served as part of your dessert."
+          menu_item.course = 'dessert'
+          break    
         else
           puts 'You entered an invalid entry. Please try again'
           sleep 2
           puts '' # Divider Line
         end
-      end
+      end      
+
+
+      # Request options
+      puts '' # Divider Line
+      print "Do you have any special options with your #{menu_item.name}? "
+      choice = gets.chomp
+      menu_item.options = choice
+      puts "Sure, I'll take note of that for your #{menu_item.name}"
+      sleep 2
+      system 'clear'
+    end
+  end  
+end
+
+def display_food_menu
+  puts '=' * 9
+  puts 'Food Menu'
+  puts '=' * 9
+  puts '' # Divider Line
+  MENU_ITEMS.each_with_index do |menu_item, index|
+    user_index = index + 1
+    # Display item with index first, then name and price
+    if menu_item.category == "Food"
+      puts "#{user_index}.) #{menu_item.name}"
+      puts "#{menu_item.description}"
+      puts "($#{menu_item.price})"
+      puts '' # Divider Line
+    end
   end
+end
+
+def display_drinks_menu
+  puts '=' * 11
+  puts 'Drinks Menu'
+  puts '=' * 11
+  puts '' # Divider Line
+  MENU_ITEMS.each_with_index do |menu_item, index|
+    user_index = index + 1
+    # Display item with index first, then name and price
+    if menu_item.category == "Drink"
+      puts "#{user_index}.) #{menu_item.name} ($#{menu_item.price})"
+      puts "#{menu_item.description}"
+      puts '' # Divider Line
+    end
+  end
+end
+
+def test(order)
+  puts order.inspect
+  gets
 end
 
 def ask_for_bill(order)
@@ -130,16 +156,16 @@ def ask_for_bill(order)
 end
 
 def main_menu
-order = Order.new
+  order = Order.new
 
   loop do
     system 'clear'
     puts 'Restaurant Main Menu'
     puts '' # Divider Line
     puts '1. Show food menu.'
-    puts '2. Order items.'
-    puts '3. Ask for bill.'
-    puts '4. Show drinks menu.'
+    puts '2. Show drinks menu.'
+    puts '3. Order items.'
+    puts '4. Ask for bill.'
     puts '' # Divider Line
     puts 'Please selection an option: '
     option = gets.chomp
@@ -148,24 +174,24 @@ order = Order.new
     when '1'
       system 'clear'
       display_food_menu
-      puts '' # Divider Line
-      puts 'Press any key to continue...'
+      puts 'Press any key...'
       gets 
     when '2'
       system 'clear'
-      order_items(order)
+      display_drinks_menu
+      puts 'Press any key...'
+      gets        
     when '3'
+      system 'clear'
+      order.request_item(order)
+    when '4'
       system 'clear'
       ask_for_bill(order)
       puts '' # Divider Line
-      puts 'Press any key to continue...'
-      gets    
-    when '4'
-      system 'clear'
-      display_drinks_menu
-      puts '' # Divider Line
-      puts 'Press any key to continue...'
-      gets         
+      puts 'Press any key...'
+      gets
+    when 't'
+      test(order)
     else 
       puts 'You have entered an invalid option. Please try again.'
       sleep 2
@@ -177,7 +203,7 @@ MENU_ITEMS = [
   MenuItem.new('Steak', 'Food', 'This boneless steak is rich, tender, juicy and full-flavored', 20),
   MenuItem.new('Parma', 'Food', 'A delicious chicken Parma', 15),
   MenuItem.new('Eggplant Casserole', 'Food', 'A healthy Turkish Eggplant Casserole Recipe with Tomatoes', 15),
-  MenuItem.new('Chips', 'Food', 'Delicious fries', 7),
+  MenuItem.new('Chips', 'Food', 'Delicious string fries cooked in batter for a nice and crispy crunch', 7),
   MenuItem.new('Beer', 'Drink', 'A nice stout beer from Germany', 7),
   MenuItem.new('Soft drink', 'Drink', 'A drink that is nice and soft', 3.50),
   MenuItem.new('Martini', 'Drink', 'The martini is a cocktail made with gin and vermouth, and garnished with an olive or a lemon twist.', 5.50),
